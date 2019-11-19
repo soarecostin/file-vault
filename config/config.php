f07af08ee92decb91cc655ea41dca1a2a51c54de
Costin Soare <soarecostin@gmail.com>
2019-11-19 17:48:33 +0100

    Improve config file

--- config/config.php
+++ config/config.php
@@ -2,20 +2,20 @@
 
 return [
     /*
-    |--------------------------------------------------------------------------
-    | Encryption Key and Cipher
-    |--------------------------------------------------------------------------
-    */
-
+     * The default key used for all file encryption / decryption
+     * This package will look for a FILE_VAULT_KEY in your env file
+     * If no FILE_VAULT_KEY is found, then it will use your Laravel APP_KEY
+     */
     'key' => env('FILE_VAULT_KEY', env('APP_KEY')),
 
+    /*
+     * The cipher used for encryption.
+     * Supported options are AES-128-CBC and AES-256-CBC
+     */
     'cipher' => 'AES-256-CBC',
 
     /*
-    |--------------------------------------------------------------------------
-    | Default storage disk
-    |--------------------------------------------------------------------------
-    */
-
+     * The Storage disk used by default to locate your files.
+     */
     'disk' => 'local',
 ];
